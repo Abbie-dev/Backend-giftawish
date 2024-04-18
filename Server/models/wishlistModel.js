9c984f31d8573451f3efb83ae01f51c65b0e8085
Nanahawaw <folarinyetunde@gmail.com>
2024-04-18 17:23:50 +0100

    category, product and wishlist schema

---
+++ Server/models/wishlistModel.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const wishlistSchema = new mongoose.schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    items: [
+        {
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product",
+                required: true
+            },
+            category: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Category",
+                required: true
+            },
+            priority: {
+                type: Number,
+                required: true
+            }
+        }
+    ]
+}, { timestamps: true })
+
+const Wishlist = mongoose.model('Wishlist', wishlistSchema);
+export default Wishlist;
