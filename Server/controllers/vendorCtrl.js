72d9df688fd6012f7878a1b5950f3f072529a1ce
Nanahawaw <folarinyetunde@gmail.com>
2024-04-19 20:58:39 +0100

    removed upload directory

--- Server/controllers/vendorCtrl.js
+++ Server/controllers/vendorCtrl.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import Product from '../models/productModel.js';
+import upload from '../config/multerConfig.js'
 
 
 export const addProduct = asyncHandler(async (req, res) => {
