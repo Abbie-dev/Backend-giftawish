b020cb4db0d3a374bb70977a832608429f1a2427
Nanahawaw <folarinyetunde@gmail.com>
2024-04-19 19:56:00 +0100

    product schema implemented

--- Server/models/productModel.js
+++ Server/models/productModel.js
@@ -1,8 +1,21 @@
 import mongoose from "mongoose";
+import slugify from "slugify";
 
 const productSchema = new mongoose.Schema({
-})
-
+    name: { type: 'string', required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    images: [{ type: String }],
+    category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
+    tags: [{ type: String }],
+    vendor: { type: mongoose.Schema.Types.ObjectId, ref: 'Vendor', required: true },
+    slug: { type: String, required: true, unique: true },
+}, { timestamps: true }
+)
+productSchema.pre('save', function (next) {
+    this.slug = slugify(this.name, { lower: true, strict: true });
+    next();
+});
 const Product = mongoose.model('Product', productSchema);
 
 export default Product;
