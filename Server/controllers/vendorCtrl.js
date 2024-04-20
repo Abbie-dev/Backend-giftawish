4bbd53278af434f78ab8083b5dcfa2d5a91c0fa2
Nanahawaw <folarinyetunde@gmail.com>
2024-04-20 13:26:25 +0100

    add product functionality

--- Server/controllers/vendorCtrl.js
+++ Server/controllers/vendorCtrl.js
@@ -1,11 +1,12 @@
 import asyncHandler from 'express-async-handler'
+import slugify from 'slugify'
 import Product from '../models/productModel.js';
 import { productImageUpload } from '../config/multerConfig.js'
 
 
 export const addProduct = asyncHandler(async (req, res) => {
     const vendor = req.vendor._id;
-    const { name, description, price, images, category, tags } = req.body;
+    const { name, description, price, category, tags } = req.body;
     try {
         // Check if a product with the same slug already exists
         const existingProduct = await Product.findOne({ slug: slugify(name, { lower: true, strict: true }) });
@@ -13,9 +14,9 @@ export const addProduct = asyncHandler(async (req, res) => {
             return res.status(400).json({ error: 'A product with the same name already exists' });
         }
 
-        const productImages = req.file.map(file => file.filename)
+        const productImages = req.files.map(file => file.filename)
         const product = new Product({ name, description, price, images: productImages, category, tags, vendor })
-
+        product.slug = slugify(product.name, { lower: true, strict: true })
         const createdProduct = await product.save()
         res.status(201).json(createdProduct)
     } catch (error) {
