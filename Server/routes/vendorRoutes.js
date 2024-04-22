e22a93d45f46b641e41c06c92159da393680e201
Nanahawaw <folarinyetunde@gmail.com>
2024-04-22 13:25:15 +0100

    handled delete product image in another file

--- Server/routes/vendorRoutes.js
+++ Server/routes/vendorRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { productImageUpload } from "../config/multerConfig.js"
 import { isVendorAuthenticated } from '../middlewares/authorization.js'
-import { addProduct, updateProduct, allProductsByVendor } from '../controllers/vendorCtrl.js'
+import { addProduct, updateProduct, allProductsByVendor, deleteImage } from '../controllers/vendorCtrl.js'
 
 const router = express.Router();
 router.post("/addProduct", isVendorAuthenticated, productImageUpload, addProduct)
 router.put('/updateProduct/:id', isVendorAuthenticated, productImageUpload, updateProduct)
+router.put('/updateProduct/:id/removeImage', isVendorAuthenticated, deleteImage)
 router.get("/allProducts", allProductsByVendor)
 export default router;
