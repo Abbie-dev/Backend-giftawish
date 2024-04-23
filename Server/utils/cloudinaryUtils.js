43361b3ee0c68e4c161a7116b730cafebe1cf4b5
Nanahawaw <folarinyetunde@gmail.com>
2024-04-23 19:10:33 +0100

    cloudinary config

--- Server/utils/cloudinaryUtils.js
+++ Server/utils/cloudinaryUtils.js
@@ -6,9 +6,12 @@ cloudinary.config(cloudinaryConfig.v2);
 export const deleteResourcesFromCloudinary = async (folder, publicIds) => {
   try {
     const resources = publicIds.map((publicId) => `${folder}/${publicId}`);
-    const deleteResult = await cloudinary.uploader.remove_by_prefix(resources, {
-      resource_type: 'image',
-    });
+    const deleteResult = await cloudinary.uploader.v2.remove_by_prefix(
+      resources,
+      {
+        resource_type: 'image',
+      }
+    );
     console.log('Deleted resources:', deleteResult);
   } catch (error) {
     console.error('Error deleting resources from Cloudinary:', error);
