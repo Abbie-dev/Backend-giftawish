af809de14ecb72cc1472a67ea0349fccf3e8b5f9
Nanahawaw <folarinyetunde@gmail.com>
2024-04-27 14:44:10 +0100

    get all products and populate with their categories

--- Server/controllers/productCtrl.js
+++ Server/controllers/productCtrl.js
@@ -38,14 +38,14 @@ export const filterProducts = asyncHandler(async (req, res) => {
 export const getAllProducts = asyncHandler(async (req, res) => {
   try {
     const products = await Product.find({})
-      .populate(category)
+      .populate('category')
       .limit(12)
-      .sort({ createAt: -1 });
+      .sort({ createdAt: -1 });
 
     res.json(products);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Server Error' });
+    res.status(500).json({ message: error.message });
   }
 });
 
