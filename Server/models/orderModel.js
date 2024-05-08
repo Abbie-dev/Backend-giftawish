01853e1cb925bdaa0a3b90289a0f48e81647f752
Nanahawaw <folarinyetunde@gmail.com>
2024-05-08 22:24:49 +0100

    order schema

--- Server/models/orderModel.js
+++ Server/models/orderModel.js
@@ -1,1 +1,14 @@
 import mongoose from 'mongoose';
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model('Order', orderSchema);
+
+export default Order;
